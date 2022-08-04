eb251a23ef545d5e96a093eacd5137c4e71d3f2f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-04 22:08:41 +0900

    axios-cheerio로 첫 크롤링하기

--- 2.xlsx-parsing-example/index.js
+++ 2.xlsx-parsing-example/index.js
@@ -1,4 +1,6 @@
 const xlsx = require('xlsx');
+const axios = require('axios'); // ajax 라이브 러리 
+const cheerio = require('cheerio'); // html 파싱
 
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 const ws = workbook.Sheets.영화목록;
@@ -12,3 +14,19 @@ for (const [i, r] of records.entries()) {
   console.log(r.제목, r.링크);
 }
 */
+
+const crawler = async () => {
+  await Promise.all(records.map(async (r) => {
+    const response = await axios.get(r.링크);
+    if (response.status === 200 ){ // 응답이 성공한 경우 
+      const html = response.data;
+      
+      const $ = cheerio.load(html);
+      const text = $('.score_left .star_score').text();
+      console.log(r.제목, '평점: ', text.trim());
+    }
+  }));
+
+}
+
+crawler();
