3babaa89bf9e678a7c5c4729df2f825825d86579
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-04 22:16:53 +0900

    Promise.all과 for of 문의 차이

--- 2.xlsx-parsing-example/index.js
+++ 2.xlsx-parsing-example/index.js
@@ -16,17 +16,21 @@ for (const [i, r] of records.entries()) {
 */
 
 const crawler = async () => {
-  await Promise.all(records.map(async (r) => {
+  for (const [i, r] of records.entries()) {
     const response = await axios.get(r.링크);
     if (response.status === 200 ){ // 응답이 성공한 경우 
       const html = response.data;
       
       const $ = cheerio.load(html);
-      const text = $('.score_left .star_score').text();
+      const text = $('.score_left .star_score').text(); // 태그는 무시하고 텍스트만 가져온다
       console.log(r.제목, '평점: ', text.trim());
     }
-  }));
+  }
 
+/*
+  await Promise.all(records.map(async (r) => {
+  }));
+*/
 }
 
 crawler();
