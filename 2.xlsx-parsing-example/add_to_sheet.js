72c4c67f5203db3d48031a74b1f5c5c8efb2c91b
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-04 23:00:21 +0900

    엑셀에 쓰기

---
+++ 2.xlsx-parsing-example/add_to_sheet.js
@@ -0,0 +1,19 @@
+//add_to_sheet.js
+const xlsx = require('xlsx');
+
+function range_add_cell(range, cell) {
+  var rng = xlsx.utils.decode_range(range);
+  var c = typeof cell === 'string' ? xlsx.utils.decode_cell(cell) : cell;
+  if (rng.s.r > c.r) rng.s.r = c.r;
+  if (rng.s.c > c.c) rng.s.c = c.c;
+
+  if (rng.e.r < c.r) rng.e.r = c.r;
+  if (rng.e.c < c.c) rng.e.c = c.c;
+  return xlsx.utils.encode_range(rng);
+}
+
+//함수에 넣을 변수들: sheet, cell, type, raw
+module.exports = function add_to_sheet(sheet, cell, type, raw) {
+  sheet['!ref'] = range_add_cell(sheet['!ref'], cell);
+  sheet[cell] = { t: type, v: raw };
+};
--- 2.xlsx-parsing-example/index.js
+++ 2.xlsx-parsing-example/index.js
@@ -1,24 +1,27 @@
 const xlsx = require('xlsx');
 const axios = require('axios'); // ajax 라이브 러리 
 const cheerio = require('cheerio'); // html 파싱
+const add_to_sheet = require('./add_to_sheet');
 
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
+const crawler = async () => {
+  add_to_sheet(ws, 'C1', 's', '평점');
 
-
-
-/* const records = xlsx.utils.sheet_to_json(ws, {header: 'A'}); // 옵션추가 
-records.shift(); // 첫번째 원소를 지워버림.
-console.log(records) */
-//console.log(ws['!ref']) // A1:B11
-
-ws['!ref'] = "A2:B11";
-const records = xlsx.utils.sheet_to_json(ws, {header: 'A'}); // 옵션추가 
-console.log(records)
-
-
-/* ws['!ref'] = ws['!ref'].split(':').map((v, i) => {
-  if (i == 0) {
-    return 'A2';
+  for (const [i, r] of records.entries()) {
+    const response = await axios.get(r.링크);
+    if (response.status === 200 ){ // 응답이 성공한 경우 
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const text = $('.score_left .star_score').text(); // 태그는 무시하고 텍스트만 가져온다
+      console.log(r.제목, '평점: ', text.trim());
+      const newCell = 'C' + (i + 2);
+      add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()));
+  
+    }
   }
-}).join(':'); */
+  xlsx.writeFile(workbook, 'xlsx/result.xlsx');
+} 
+
+crawler();
