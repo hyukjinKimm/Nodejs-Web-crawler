a23ea6a25b024970ca9a85f7f78861bcc8357cb1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-05 13:48:16 +0900

    headless 옵션 이해하기

--- 3.csv-puppeteer-multipage/index.js
+++ 3.csv-puppeteer-multipage/index.js
@@ -6,16 +6,23 @@ const csv = fs.readFileSync('csv/data.csv');
 const records = parse(csv.toString('utf-8'));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  const page2 = await browser.newPage();
-  const page3 = await browser.newPage();
-  await page.goto('https://zerocho.com');
-  await page2.goto('https://naver.com');
-  await page3.goto('https://google.com');
-  await page.waitForTimeout(1000);
-  await page2.waitForTimeout(2000);
-  await page3.waitForTimeout(3000);
+  const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production' });
+  
+  const [page, page2, page3] =   await Promise.all([
+    browser.newPage(),
+    browser.newPage(),
+    browser.newPage()
+  ])
+  await Promise.all([
+    page.goto('https://zerocho.com'),
+    page2.goto('https://naver.com'),
+    page3.goto('https://google.com')
+  ]);
+  await Promise.all([
+    page.waitForTimeout(1000),
+    page2.waitForTimeout(2000),
+    page3.waitForTimeout(3000)
+  ]);
   await page.close();
   await page2.close();
   await page3.close();
