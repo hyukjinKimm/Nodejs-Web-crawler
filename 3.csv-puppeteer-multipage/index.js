eb32b3b7c4376f6b6f1745bf3a8e9c8a69ac4937
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-06 17:15:02 +0900

    csv에 출력하기

--- 3.csv-puppeteer-multipage/index.js
+++ 3.csv-puppeteer-multipage/index.js
@@ -1,4 +1,5 @@
 const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify/lib/sync');
 const fs = require('fs');
 const puppeteer = require('puppeteer'); 
 
@@ -6,7 +7,10 @@ const csv = fs.readFileSync('csv/data.csv');
 const records = parse(csv.toString('utf-8'));
 
 const crawler = async () => {
+
   try{
+    const result = [];
+
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production' });
     await Promise.all(records.map(async (r, i) => {
       try{
@@ -16,14 +20,19 @@ const crawler = async () => {
         if (scoreE1) {
           const text = await page.evaluate(tag => tag.textContent, scoreE1)
           console.log(r[0], " 평점:",text.trim());
+          // result.push([r[0], r[1], text.trim()]); 원본 data 와 저장 data 의 순서 보장이 안됨.
+          result[i] = [r[0], r[1], text.trim()];
+
         }
         await page.close();
       } catch(e){
           console.error(e);
       }
 
-    }))
+    }));
   await browser.close();
+  const str = stringify(result);
+  fs.writeFileSync('csv/result.csv',str);
   } catch(e) {
     console.error(e);
   }
