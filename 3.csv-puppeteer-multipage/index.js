16fb62232d055d7bbd1659e5bb65f533891aa93c
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-06 18:10:43 +0900

    xlsx - userAgent와 한 탭으로 크롤링

--- 3.csv-puppeteer-multipage/index.js
+++ 3.csv-puppeteer-multipage/index.js
@@ -1,20 +1,21 @@
-const parse = require('csv-parse/lib/sync');
-const stringify = require('csv-stringify/lib/sync');
-const fs = require('fs');
+const xlsx = require('xlsx');
 const puppeteer = require('puppeteer'); 
+const add_to_sheet = require('./add_to_sheet'); 
 
-const csv = fs.readFileSync('csv/data.csv');
-const records = parse(csv.toString('utf-8'));
+const workbook = xlsx.readFile('xlsx/data.xlsx');
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
 
 const crawler = async () => {
 
   try{
-    const result = [];
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production' });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36');
+    add_to_sheet(ws, 'C1', 's', '평점');
+
     for ( const [i, r] of records.entries()){
-      await page.goto(r[1]);
+      await page.goto(r.링크);
       // const 태그핸들러 = await page.$(선택자);
       const text = await page.evaluate(() => { //document 를 쓸 수 있다.
         const score = document.querySelector('.score_left .star_score');
@@ -23,15 +24,15 @@ const crawler = async () => {
         }
       });
       if ( text ) {
-        console.log(r[0], '평점', text.trim() );
-        result[i] = [r[0], r[1], text.trim()];
+        console.log(r.제목, '평점', text.trim() );
+        const newCell = 'C' + (i + 2);
+        add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()));
       }
       await page.waitForTimeout(1000);
     };
     await page.close();
     await browser.close();
-    const str = stringify(result);
-    fs.writeFileSync('csv/result.csv',str);
+    xlsx.writeFile(workbook, 'xlsx/result.xlsx');
   } catch(e) {
     console.error(e);
   }
