1a24a713657708312ca6ee70c0174d8ea7b84061
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-11 16:11:12 +0900

    스크롤 조작해서 크롤링하기

--- 7.image-scroll/index.js
+++ 7.image-scroll/index.js
@@ -7,27 +7,34 @@ const crawler = async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://unsplash.com');
-    const result = await page.evaluate(() => {
-      const images = []; 
-
-      //const imgEls = document.querySelectorAll('.ripi6  img.YVj9w');
-      const imgEls = document.querySelectorAll('.VQW0y');
-      if(imgEls.length) { 
-        imgEls.forEach((v) => {
-          let src = v.querySelector('img.YVj9w').src;
-          if(src) {
-            images.push(src);
+    let result = [];
+    while(result.length <= 60) {
+        const srcs = await page.evaluate(() => {
+            window.scrollTo(0, 0);
+            let images = []; 
+            //const imgEls = document.querySelectorAll('.ripi6  img.YVj9w');
+            const imgEls = document.querySelectorAll('.VQW0y');
+            if(imgEls.length) { 
+              imgEls.forEach((v) => {
+                let src = v.querySelector('img.YVj9w').src;
+                if(src) {
+                  images.push(src);
+                }
+                v.parentElement.removeChild(v);
+              });
+              window.scrollBy(0, 300);
+              return images;
+            }
           }
-          v.parentElement.removeChild(v);
-        });
-        window.scrollBy(0, 300);
-        return images;
-      }
+          );
+          result = result.concat(srcs);
+          console.log(result.length)
+          await page.waitForSelector('.VQW0y');
+          console.log('새 이미지 태그 로딩 완료!');
     }
-    );
-    console.log(result);
-    await page.waitForSelector('.VQW0y');
-    console.log('태그 로딩 완료!');
+    await page.close();
+    await browser.close();    
+
   } catch (e) {
     console.error(e);
   }
