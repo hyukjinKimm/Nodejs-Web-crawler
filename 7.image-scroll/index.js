401318cdc94fe37c5c5fefc9a85ff9a4782ce08a
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-11 16:30:16 +0900

    크롤링 결과를 파일로 만들기

--- 7.image-scroll/index.js
+++ 7.image-scroll/index.js
@@ -2,13 +2,19 @@ const puppeteer = require('puppeteer');
 const axios = require('axios');
 const fs = require('fs');
 
+fs.readdir('images', (err) => {
+    if (err) {
+      console.error('images 폴더가 없어 images 폴더를 생성합니다.');
+      fs.mkdirSync('images');
+    }
+  });
 const crawler = async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://unsplash.com');
     let result = [];
-    while(result.length <= 60) {
+    while(result.length <= 30) {
         const srcs = await page.evaluate(() => {
             window.scrollTo(0, 0);
             let images = []; 
@@ -28,10 +34,17 @@ const crawler = async () => {
           }
           );
           result = result.concat(srcs);
-          console.log(result.length)
+          console.log(result)
           await page.waitForSelector('.VQW0y');
           console.log('새 이미지 태그 로딩 완료!');
     }
+    result.forEach(async (src) => {
+      const imageResult = await axios.get(src.replace(/\?.*$/, ''), {
+        responseType: 'arraybuffer',
+      });
+      fs.writeFileSync(`images/${new Date().valueOf()}.jpg`, imageResult.data); // 현재시간을 파일 이름으로 
+
+    });
     await page.close();
     await browser.close();    
 
