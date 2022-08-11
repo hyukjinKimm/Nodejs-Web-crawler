7ed7d619ef5597f8861f2871b609bb2c6198d129
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-11 15:51:18 +0900

    스크롤 내리고 태그 기다리기

--- 7.image-scroll/index.js
+++ 7.image-scroll/index.js
@@ -8,21 +8,26 @@ const crawler = async () => {
     const page = await browser.newPage();
     await page.goto('https://unsplash.com');
     const result = await page.evaluate(() => {
-      const images = [];
-      const imgEls = document.querySelectorAll('.ripi6  img.YVj9w');
+      const images = []; 
+
+      //const imgEls = document.querySelectorAll('.ripi6  img.YVj9w');
+      const imgEls = document.querySelectorAll('.VQW0y');
       if(imgEls.length) { 
         imgEls.forEach((v) => {
-          if(v.src) {
-            images.push(v.src);
+          let src = v.querySelector('img.YVj9w').src;
+          if(src) {
+            images.push(src);
           }
+          v.parentElement.removeChild(v);
         });
+        window.scrollBy(0, 300);
         return images;
       }
     }
     );
     console.log(result);
-    await page.close();
-    await browser.close();
+    await page.waitForSelector('.VQW0y');
+    console.log('태그 로딩 완료!');
   } catch (e) {
     console.error(e);
   }
