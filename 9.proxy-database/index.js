ed9f994639789a4769cf0085b9ca9df48c62567d
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-16 02:00:05 +0900

    프록시 ip 적용하기

--- 9.proxy-database/index.js
+++ 9.proxy-database/index.js
@@ -2,11 +2,11 @@ const puppeteer = require('puppeteer');
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({
+    let browser = await puppeteer.launch({
       headless: false,
       args: ['--window-size=1920,1080', '--disable-notifications'],
     });
-    const page = await browser.newPage();
+    let page = await browser.newPage();
     await page.setViewport({
       width: 1080,
       height: 1080,
@@ -14,21 +14,35 @@ const crawler = async () => {
 
    await page.goto('https://spys.one/en/');
    const proxies = await page.evaluate(() => {
-    const a = document.querySelectorAll('tr > td:first-child > .spy14 ')[0].innerText;  
+    const ips = Array.from(document.querySelectorAll('tr > td:first-child > .spy14 ')).map((v) => v.innerText);  
 
-    const b = document.querySelectorAll('tr > td:nth-child(2) .spy1')[0].innerText 
+    const types = Array.from(document.querySelectorAll('tr > td:nth-child(2) .spy1')).map((v) => v.innerText);
     
-    const c = Array.from( document.querySelectorAll('tr > td:nth-child(5) .spy1 acronym')).map((item) => {
+    const uptimes = Array.from( document.querySelectorAll('tr > td:nth-child(5) .spy1 acronym')).map((item) => {
       if(item.innerText.search("[0-9]+%") == -1)
         return "0"
       else
          return item.innerText.match("[0-9]+%")["0"].replace("%","");
     });
     
-   })
+    return ips.map((v, i) => {
+      return {
+      ip : v,
+      type : types[i],
+      uptime : uptimes[i] 
+      }
+    });
+   });
+   const filtered =  proxies.filter((v) => v.type.startsWith('HTTP')).sort((p, c) => c.uptime - p.uptime);
 
    await page.close();
    await browser.close();
+   browser = await puppeteer.launch({
+    headless: false,
+    args: ['--window-size=1920,1080', '--disable-notifications', `--proxy-server=${filtered[0].ip}`],
+  });
+
+   page = await browser.newPage();
 
   } catch (e) {
     console.error(e);
