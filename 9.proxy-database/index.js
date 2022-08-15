902e46931b069e54cba49ea619535b4a2009e984
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-16 02:16:13 +0900

    데이터베이스 연동하기

--- 9.proxy-database/index.js
+++ 9.proxy-database/index.js
@@ -1,5 +1,15 @@
 const puppeteer = require('puppeteer');
+const { sequelize } = require('./models');
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
+
+  
 const crawler = async () => {
   try {
     let browser = await puppeteer.launch({
---
+++ 9.proxy-database/models/proxy.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Proxy extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      ip: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      type: {
+         type: Sequelize.STRING(20),
+         allowNull: false,
+      },
+      uptime: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: 'Proxy',
+      tableName: 'proxys',
+      paranoid: false,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+  }
+}
