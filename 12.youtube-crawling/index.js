4b28464622646d799c8946ed55561cf78a738095
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-02 19:49:44 +0900

    구글 로그인

--- 12.youtube-crawling/index.js
+++ 12.youtube-crawling/index.js
@@ -16,7 +16,9 @@ const crawler  = async () => {
         args: ["--window-size=1920,700", "--disable-notifications"],
         userDataDir: './data'
      });
+     
      const page = await browser.newPage();
+     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36');
      await page.setViewport({
          width: 700,
          height: 900
@@ -25,11 +27,24 @@ const crawler  = async () => {
      await page.goto('https://youtube.com', {
         waitUntil: 'networkidle0' // 모든 네트워크 요청이 마무리 되야 다음 코드로 진행.
      });
-     await page.waitForSelector('#buttons ytd-button-renderer:last-child a');
-     await page.click('#buttons ytd-button-renderer:last-child a');
-     await page.waitForNavigation({
-        waitUntil: 'networkidle2' // 2개정도의 네트워크 요청은 덜 마무리 되어도 다음 코드로 진행
-     })
+     if(!await page.$('avatar-btn')){
+        await page.waitForSelector('#buttons ytd-button-renderer:last-child a');
+        await page.click('#buttons ytd-button-renderer:last-child a');
+        await page.waitForNavigation({
+           waitUntil: 'networkidle2' // 2개정도의 네트워크 요청은 덜 마무리 되어도 다음 코드로 진행
+        });
+        await page.waitForSelector('#identifierId');
+        await page.type('#identifierId', process.env.GOOGLE_ID);
+        await page.waitForSelector('#identifierNext');
+        await page.click('#identifierNext')
+        /**
+         * 
+         *  로그인 코드 작성
+         */
+     } else {
+        console.log('이미 로그인 됨')
+     }
+
      // await page.close();
      // await browser.close();
   } catch(e) {
